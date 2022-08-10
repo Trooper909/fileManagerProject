550f75722b7936eb9c801654b36617b421d66b41
Trooper909 <93057567+Trooper909@users.noreply.github.com>
2022-08-11 00:04:26 +0530

    Update mod2OrganiseRoutine.js

--- fileManagerproject/mod2OrganiseRoutine.js
+++ fileManagerproject/mod2OrganiseRoutine.js
@@ -8,7 +8,7 @@ const {makeSure} = require(".\\mod4MakeSure.js");
 
 //actual program
 /**
- 1. stage 1: "organise" folder namae check
+ 1. stage 1: "organise" folder name check
  2. stage 2: filecheck and segregation in to specific type and others type
  2. stage 3: sending files to the respective locations
 */
